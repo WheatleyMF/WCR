6238424f1b981415a07e38a09b207543efe6bc04
WheatleyMF <wheatleyinthespace@yandex.ru>
2020-10-31 23:16:16 +0300

    folders, placeholder images and basic html/js/css
    ignore the chaos energy of those 2 files..

---
+++ code/reader.js
@@ -0,0 +1,3 @@
+console.log("yes I am working, now fuck off");
+// tl;dr load up files, cache them somehow, implement with UI to scroll around and blah blah
+// I have the concept in my mind but it's hard to move it on "paper" currently
